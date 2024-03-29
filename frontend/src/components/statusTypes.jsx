397dd1af27100f0cc335fdaab22968e4036a78ce
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-03-29 18:11:32 +0530

    Update statusTypes.jsx

--- frontend/src/components/statusTypes.jsx
+++ frontend/src/components/statusTypes.jsx
@@ -4,7 +4,7 @@ const StatusTypes = ({content}) => {
     return (
         <div className='status-container'>
                 {content.type === 'image' && <img src={content.url}/>}
-                {content.type === 'video' && <video autoPlay  controls controlslist="nofullscreen nodownload  noplaybackrate"><source src={content.url} /></video>}
+                {content.type === 'video' && <video className='status-video' autoPlay  controls controlslist="nofullscreen nodownload  noplaybackrate"><source src={content.url} /></video>}
                 {content.type == 'text' && <span style={{textAlign:'center'}}>{content.text}</span>}
         </div>
     )
