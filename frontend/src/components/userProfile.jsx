ba14dc17b4b5ac40bda8687f14d09ac316f07cb2
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-03-30 21:35:14 +0530

    Update userProfile.jsx

--- frontend/src/components/userProfile.jsx
+++ frontend/src/components/userProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import '../cssFile/userProfile.css';
 import { selectedChat } from '../context/selectedChat';
 import { useSelectUserId } from '../context/userInfoContext';
-import {usesocketIoContext} from '../context/socketIo'
-const UserProfile = ({ userData,  chnage_show }) => {
+import { usesocketIoContext } from '../context/socketIo'
+const UserProfile = ({ userData, chnage_show }) => {
   const { setdata4 } = useSelectUserId()
-  const {onlineUser} = usesocketIoContext()
+  const { onlineUser } = usesocketIoContext()
 
   const chhange_user_info = () => {
     setdata4(userData)
@@ -16,16 +16,20 @@ const UserProfile = ({ userData,  chnage_show }) => {
   // prevent from clicking again on profile
   const changeRight = () => {
     if (data2?._id != userData._id) {
-      setdata2({ fullName: userData.fullName, profilePic: userData.profilePic, _id: userData._id, message: [],status:userData?.status })
+      setdata2({ fullName: userData.fullName, profilePic: userData.profilePic, _id: userData._id, message: [], status: userData?.status })
     }
   }
-  
+
   const isUserOnline = onlineUser.includes(userData._id);
   return (
     <div className='User-profile-container' onClick={e => changeRight()} style={{ backgroundColor: (data2?._id === userData?._id) && '#334560' }}>
-      {isUserOnline&&<div className='online'></div>}
-      <img loading='lazy' style={{outline:userData.status.type&&'3px solid red'}} src={userData.profilePic} onClick={chhange_user_info} />
-      <span className='name'>{userData.fullName}</span>
+      {isUserOnline && <div className='online'></div>}
+      <img loading='lazy' style={{ outline: userData.status.type && '3px solid red' }} src={userData.profilePic} onClick={chhange_user_info} />
+      <div className='User-profile-container-name-last-message'>
+        <span className='name'>{userData.fullName}</span>
+        <span className='last-message'>last message</span>
+        
+      </div>
       <span className='rocket'>🚀</span>
     </div>
   )
