baa6f08ff91ebbceecf7ca4676ccf2847c93d551
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-01 19:31:16 +0530

    Update userSearch.jsx

--- frontend/src/components/userSearch.jsx
+++ frontend/src/components/userSearch.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
+
+import React, { useState } from 'react';
+import {Add} from './svg';
 import '../cssFile/userProfile.css';
 import {useAuth} from '../context/AuthUser';
+import SearchUser from './searchUser'
 import {useSelectUserId} from '../context/userInfoContext';
 const UserSearch = ({chnage_show,Search_user}) => {
   const {setdata4} = useSelectUserId();
   const {authuser} = useAuth();
+  const[showSearch,setShowSearch] = useState(false)
 const chhange_value = ()=>{
   setdata4(authuser);
   chnage_show();
+}
+const AddUser = ()=>{
+   setShowSearch(!showSearch)
 }
 const [data,setData] = useState('')
   return (
     <div className='user-search-profile'>
       <img src={authuser?.profilePic} loading='lazy' onClick={chhange_value}/>
       <input onChange={e=>{Search_user(e.target.value);setData(e.target.value);}} type='text' placeholder='Search User...' value={data}/>
-      <button onClick={e=>Search_user(data)}>
-      </button>
+      <Add AddUser={AddUser}/>
+      {showSearch&&<div>
+      <SearchUser/>
+      </div>
+}
+     
     </div>
   )
 }
