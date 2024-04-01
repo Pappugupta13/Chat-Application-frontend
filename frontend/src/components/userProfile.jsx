bb4af2db1ef26d948322210e0c8dbb551079713b
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-01 19:32:06 +0530

    Update userProfile.jsx

--- frontend/src/components/userProfile.jsx
+++ frontend/src/components/userProfile.jsx
@@ -3,7 +3,7 @@ import '../cssFile/userProfile.css';
 import { selectedChat } from '../context/selectedChat';
 import { useSelectUserId } from '../context/userInfoContext';
 import { usesocketIoContext } from '../context/socketIo'
-const UserProfile = ({ userData, chnage_show }) => {
+const UserProfile = ({ userData, chnage_show,lastMessage }) => {
   const { setdata4 } = useSelectUserId()
   const { onlineUser } = usesocketIoContext()
 
@@ -27,7 +27,7 @@ const UserProfile = ({ userData, chnage_show }) => {
       <img loading='lazy' style={{ outline: userData.status.type && '3px solid red' }} src={userData.profilePic} onClick={chhange_user_info} />
       <div className='User-profile-container-name-last-message'>
         <span className='name'>{userData.fullName}</span>
-        <span className='last-message'>last message</span>
+        <span className='last-message'>{lastMessage}</span>
         
       </div>
       <span className='rocket'>🚀</span>
