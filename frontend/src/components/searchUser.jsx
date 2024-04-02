cafde5b2cde76f06a49a04f1c4a6df726afcad31
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-02 23:14:51 +0530

    Update searchUser.jsx

--- frontend/src/components/searchUser.jsx
+++ frontend/src/components/searchUser.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {searchUser} from '../hooks/convertSize'
+import '../cssFile/profileViewer.css';
+import UserProfile from './userProfile';
+const SearchUser = ({ AddUser }) => {
 
-const SearchUser = () => {
+  const [inputvalue, setInputValue] = useState(null)
+  const getData = async ()=>{
+    const x = await searchUser(inputvalue)
+  }
   return (
-    <div className='profile-viewer'>
-          <h1>eeeeeeeeee</h1>
+    <div className='profile-viewer' style={{ position: 'absolute', height: '100vh', zIndex: '10' }}>
+      <div className='user-search-profile' style={{ marginTop: '15px',background:'none',width:'97%',borderRadius:'0',borderBottom:'3px solid black'}}>
+      <input type='text' id='main-search-input-tag' style={{width:'80%'}} placeholder='search the user...' onChange={e=>setInputValue(e.target.value)}/>
+      <button onClick={getData}></button>
+        <svg onClick={AddUser} aria-label="Close" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Close</title><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="21" y2="3"></line></svg>
+      </div>
+      <div style={{height:'100%',overflowY:'auto'}}>
+
+      </div>
     </div>
   )
 }
