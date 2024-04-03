6dd8f4885f698f2a8a438d188507459f77dad5e5
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-03 11:54:52 +0530

    Update convertSize.js

--- frontend/src/hooks/convertSize.js
+++ frontend/src/hooks/convertSize.js
@@ -18,20 +18,16 @@ const convertSize = (fileSizeInBytes) => {
 const url = import.meta.env.VITE_APP_API_URL;
 export const searchUser = async (search) => {
   try{
-    const response = await fetch(`${url}/users`,{
+    const response = await fetch(`${url}/users?search=${search}`,{
       method: 'GET',
       credentials: 'include',
-      body: JSON.stringify({
-        inputValue:search
-    })
     });
     const data = await response.json();
     if(data.error || !data){
       alert("Something went  wrong!");
       return console.log("Something went  wrong!");
     }
-    console.log(data);
-    // return  data;
+    return  data;
   }
   catch(e){
     console.log("error "+e);
