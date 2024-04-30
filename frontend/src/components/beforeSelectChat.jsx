30500ce0bc77fb67e679f62fadc6b82287e1c578
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-30 12:24:05 +0530

    Update beforeSelectChat.jsx

--- frontend/src/components/beforeSelectChat.jsx
+++ frontend/src/components/beforeSelectChat.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../cssFile/beforeSelectChat.css';
+import {useUserContext} from '../context/themeContext'
 const BeforeSelectChat = () => {
+  const {data} = useUserContext()
   return (
-    <div className='before-selected-chat'>
+    <div className='before-selected-chat' style={{backgroundColor:data.beforeColor}}>
         <div className='before-selected-chat-bg-image-text'>
       <div className='bg-for-before-selected-chat'></div>
       <span>select the chat for conversation</span>
