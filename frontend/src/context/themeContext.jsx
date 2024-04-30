a3bc80ce898e255332b8677b566ba60d1d9f866d
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-30 12:22:50 +0530

    Update themeContext.jsx

--- frontend/src/context/themeContext.jsx
+++ frontend/src/context/themeContext.jsx
@@ -7,12 +7,15 @@ export const useUserContext = ()=>{
   return useContext(UserContext);
 }
 const ThemeContext = ({children}) => {
+ const {leftBg,rightTop,rightMessage,rightBottom,image,beforeColor} = JSON.parse(localStorage.getItem('chat-theme'))||{};
   const [data,setdata] = useState({
-    leftBg:'#F9F7F3',
-    rightTop:'#f0f2f5',
-    rightMessage:'#e3d1b4',
-    rightBottom:'#f0f2f5'
-  });
+    leftBg:leftBg||'#F9F7F3',
+    rightTop:rightTop||'#f0f2f5',
+    rightMessage:rightMessage||'#e3d1b4',
+    rightBottom: rightBottom||'#f0f2f5',
+    image:image||'https://cdn-icons-png.freepik.com/512/6714/6714978.png',
+    beforeColor:beforeColor||'#E5E5E5'
+  }); 
   return (
    <UserContext.Provider value={{data,setdata}}>
     {children}
