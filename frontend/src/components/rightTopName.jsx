d0d283cd7c0c637e9948b0a1333267d71aad4ecf
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-04-30 12:23:33 +0530

    Update rightTopName.jsx

--- frontend/src/components/rightTopName.jsx
+++ frontend/src/components/rightTopName.jsx
@@ -1,40 +1,59 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import '../cssFile/rightTopName.css';
-import { selectedChat  } from '../context/selectedChat';
+import { selectedChat } from '../context/selectedChat';
 import { useUserContext } from '../context/themeContext';
 const RightTopName = () => {
-  const {data,setdata} = useUserContext();
-  const {data2} = selectedChat();
-  const [datas,setdatas] = useState({image:'https://cdn-icons-png.freepik.com/512/6714/6714978.png',change:true});
-  const change_theme = (e)=>{
-    if(datas.change){
-      setdatas({...datas,image:'https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-sun-icon-png-image_4224147.jpg',change:false});
-      setdata({...data,leftBg:'#6B7280',
-      rightTop:'#374151',
-      rightMessage:'#4C5563',
-      rightBottom:'#374151'
-    });
+  const { data, setdata } = useUserContext();
+  const { data2 } = selectedChat();
+  const change_theme = (e) => {
+    if (data.image === 'https://cdn-icons-png.freepik.com/512/6714/6714978.png') {
+      setdata({
+        ...data, leftBg: '#6B7280',
+        rightTop: '#374151',
+        rightMessage: '#4C5563',
+        rightBottom: '#374151',
+        image: 'https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-sun-icon-png-image_4224147.jpg',
+        beforeColor:'#575e6c'
+      });
+      localStorage.setItem("chat-theme",JSON.stringify({
+        ...data, leftBg: '#6B7280',
+        rightTop: '#374151',
+        rightMessage: '#4C5563',
+        rightBottom: '#374151',
+        image: 'https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-sun-icon-png-image_4224147.jpg',
+        beforeColor:'#575e6c'
+      }))
     }
-    else{
-      setdata({...data,  
-        leftBg:'#F9F7F3',
-        rightTop:'#f0f2f5',
-        rightMessage:'#e3d1b4',
-        rightBottom:'#f0f2f5'
-    });
-      setdatas({...datas,image:'https://cdn-icons-png.freepik.com/512/6714/6714978.png',change:true})
-
+    else {
+      setdata({
+        ...data,
+        leftBg: '#F9F7F3',
+        rightTop: '#f0f2f5',
+        rightMessage: '#e3d1b4',
+        rightBottom: '#f0f2f5',
+        image: 'https://cdn-icons-png.freepik.com/512/6714/6714978.png',
+        beforeColor:'#E5E5E5'
+      });
+      localStorage.setItem("chat-theme",JSON.stringify({
+        ...data,
+        leftBg: '#F9F7F3',
+        rightTop: '#f0f2f5',
+        rightMessage: '#e3d1b4',
+        rightBottom: '#f0f2f5',
+        image: 'https://cdn-icons-png.freepik.com/512/6714/6714978.png',
+        beforeColor:'#E5E5E5'
+      }))
     }
   }
   return (
-    <div className='right-top-container' style={{backgroundColor:data.rightTop}}>
-      <div style={{display:'flex',alignItems:'center',marginLeft:'20px',gap:'10px'}}>
+    <div className='right-top-container' style={{ backgroundColor: data.rightTop }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginLeft: '20px', gap: '10px' }}>
         <img src={data2.profilePic} className='profile-pic-for-right' alt="Profile Picture" />
-        <span className="name" style={{color:data.rightTop==='#374151'&&'white'}}>{data2.fullName}</span>
+        <span className="name" style={{ color: data.rightTop === '#374151' && 'white' }}>{data2.fullName}</span>
 
       </div>
-      <div style={{marginRight:'15px'}}>
-        <img onClick={change_theme} src={datas.image} className='dark-light-mood' />
+      <div style={{ marginRight: '15px' }}>
+        <img onClick={change_theme} src={data.image} className='dark-light-mood' />
       </div>
     </div>
   )
