13a364833d3ba07ebbe87583cc561f90458f3d82
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-08 13:51:58 +0530

    Update notification.jsx

--- frontend/src/game/notification.jsx
+++ frontend/src/game/notification.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React,{useEffect, useState,useRef} from 'react';
 import '../cssFile/notification.css';
 import { Cross } from '../components/svg';
+import { usesocketIoContext } from '../context/socketIo';
 const Notification = () => {
-  const g = Array(2).fill("l")
+  const isSame = useRef(null)
+const [reqUser,setReqUser] = useState([])
+  const { socket } = usesocketIoContext();
+  const [show,setShow]  = useState({
+    parent:true,
+    Children:false
+  })
+  const hideShow = ({which})=>{
+   if(which === "parent"){
+    return setShow({...show,parent:!parent})
+   }
+  }
+  useEffect(()=>{
+    socket?.on("isAccept",async ({name,profilePic,id})=>{
+      setReqUser((prevReqUser) => [...prevReqUser, { name, profilePic,id }]);
+    })
+    return ()=>{
+      socket?.off("isAccept")
+    }
+  },[socket])
+ 
+
+
   return (
-    <div className='notification-container'>
+    <>
+    <div className='main-container-for-animation'>
+   {show.parent&&<div  className='notification-container'>
       <div style={{display:'flex',justifyContent:'space-around',alignItems:'center'}}>
       <span style={{fontSize:20,marginLeft:5,color:'white'}}>Ask to join Game</span>
-      <Cross height={20} width={20}/>
+      <label style={{display:'flex'}} onClick={e=> hideShow({which:"parent"})}><Cross height={20} width={20}/></label>
       </div>
       <div className='notify-child-container'>
-      {g.map(()=>(<div className='notify-child'>
+      {reqUser&&reqUser.map((item,index)=>(<div className='notify-child'>
             <div className='first-notify-child'>
                 <div className='User-profile-container-name-last-message'>
-                <img src='https://avatar.iran.liara.run/username?username=test+undefined'/>
-                <span className='last-message' style={{fontSize:17,width:110,color:'black'}}>Nitesh kumar</span>
-                <Cross height={15} width={15}/>
+                <img src={item.profilePic}/>
+                <span className='last-message' style={{fontSize:17,width:110,color:'black'}}>{item.name}</span>
+                <label onClick={3}><Cross height={15} width={15}/></label>
                 </div>
             </div>
             <div className='button-container'>
@@ -24,7 +49,9 @@ const Notification = () => {
             </div>
         </div>))}
       </div>
+    </div>}
     </div>
+    </>
   )
 }
 
