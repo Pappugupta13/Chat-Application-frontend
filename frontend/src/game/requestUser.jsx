7784abf724ecc82397e22aed5941299de51e29cc
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-08 13:51:21 +0530

    Update requestUser.jsx

--- frontend/src/game/requestUser.jsx
+++ frontend/src/game/requestUser.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import '../cssFile/userProfile.css';
+import { usesocketIoContext } from '../context/socketIo';
 import sent from '../image/invite.png';
 const RequestUser = ({userData,updateSent,sentinvite}) => {
+  const { socket } = usesocketIoContext();
+  const isSame = useRef(null)
    const isTrue = sentinvite.includes(userData._id)
+   if(isTrue && isSame.current !== userData._id){
+    isSame.current = userData._id
+    const {fullName,profilePic} = JSON.parse(localStorage.getItem('demo-chat-user'));
+    socket?.emit("request",{id:userData._id,reqName:fullName,profilePic});
+   
+   }
   return (
     <div className='User-profile-container' style={{width:'235px',height:'30px'}} onClick={e=>updateSent({_id:userData._id})}>
         <img loading='lazy' style={{maxHeight:'35px',minHeight:'auto',maxWidth:'35px',minWidth:'auto'}} src={userData.profilePic}/>
         <div className='User-profile-container-name-last-message' style={{width:'180px'}}>
-            <span className='last-message' style={{fontSize:20}}>{userData.fullName}abcdefghijkl</span>
+            <span className='last-message' style={{fontSize:20}}>{userData.fullName}</span>
       </div>
       {isTrue&&<img src={sent} style={{height:20,width:20}}/>}
     </div>
