3fbbf93b4a47426e934052bd18304086c9805d78
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-09 18:10:28 +0530

    Update notification.jsx

--- frontend/src/game/notification.jsx
+++ frontend/src/game/notification.jsx
@@ -1,56 +1,60 @@
-import React,{useEffect, useState,useRef} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../cssFile/notification.css';
 import { Cross } from '../components/svg';
 import { usesocketIoContext } from '../context/socketIo';
 const Notification = () => {
-  const isSame = useRef(null)
-const [reqUser,setReqUser] = useState([])
+  const [reqUser, setReqUser] = useState([])
   const { socket } = usesocketIoContext();
-  const [show,setShow]  = useState({
-    parent:true,
-    Children:false
+  const [show, setShow] = useState({
+    parent: true,
+    Children: false
   })
-  const hideShow = ({which})=>{
-   if(which === "parent"){
-    return setShow({...show,parent:!parent})
-   }
+  const hideShow = ({ which }) => {
+    if (which === "parent") {
+      return setShow({ ...show, parent: !parent })
+    }
   }
-  useEffect(()=>{
-    socket?.on("isAccept",async ({name,profilePic,id})=>{
-      setReqUser((prevReqUser) => [...prevReqUser, { name, profilePic,id }]);
+  const isAcceptORCancel = ({ isyes, id }) => {
+    if (isyes === "yes") {
+      const userName = JSON.parse(localStorage.getItem('demo-chat-user')).fullName;
+      socket?.emit("isAccept", { istrue: "yes", id, Name: userName });
+
+    }
+  }
+
+  useEffect(() => {
+    socket?.on("isrequest", async ({ name, profilePic, id }) => {
+      setReqUser((prevReqUser) => [...prevReqUser, { name, profilePic, id }]);
     })
-    return ()=>{
+    return () => {
       socket?.off("isAccept")
     }
-  },[socket])
- 
-
-
+  }, [socket])
   return (
     <>
-    <div className='main-container-for-animation'>
-   {show.parent&&<div  className='notification-container'>
-      <div style={{display:'flex',justifyContent:'space-around',alignItems:'center'}}>
-      <span style={{fontSize:20,marginLeft:5,color:'white'}}>Ask to join Game</span>
-      <label style={{display:'flex'}} onClick={e=> hideShow({which:"parent"})}><Cross height={20} width={20}/></label>
-      </div>
-      <div className='notify-child-container'>
-      {reqUser&&reqUser.map((item,index)=>(<div className='notify-child'>
-            <div className='first-notify-child'>
+      <div className='main-container-for-animation'>
+        {show.parent && reqUser.length !== 0 && <div className='notification-container'>
+          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <span style={{ fontSize: 20, marginLeft: 5, color: 'white' }}>Ask to join Game</span>
+            <label style={{ display: 'flex' }} onClick={e => hideShow({ which: "parent" })}><Cross height={20} width={20} /></label>
+          </div>
+          <div className='notify-child-container'>
+            {reqUser && reqUser.map((item, index) => (<div className='notify-child'>
+              <div className='first-notify-child'>
                 <div className='User-profile-container-name-last-message'>
-                <img src={item.profilePic}/>
-                <span className='last-message' style={{fontSize:17,width:110,color:'black'}}>{item.name}</span>
-                <label onClick={3}><Cross height={15} width={15}/></label>
+                  <img src={item.profilePic} />
+                  <span className='last-message' style={{ fontSize: 17, width: 110, color: 'black' }}>{item.name}</span>
+                  <label onClick={3}><Cross height={15} width={15} /></label>
                 </div>
-            </div>
-            <div className='button-container'>
-                <button>Yes</button>
-                <button>No</button>
-            </div>
-        </div>))}
+              </div>
+              <div className='button-container'>
+                <button onClick={e => isAcceptORCancel({ isyes: "yes", id: item.id })}>Yes</button>
+                <button onClick={e => isAcceptORCancel("No")}>No</button>
+              </div>
+            </div>))}
+          </div>
+        </div>}
       </div>
-    </div>}
-    </div>
     </>
   )
 }
