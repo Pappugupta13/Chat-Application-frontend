3bd221d453d5887c105007092c2d711938e54fca
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-10 15:06:30 +0530

    Update notification.jsx

--- frontend/src/game/notification.jsx
+++ frontend/src/game/notification.jsx
@@ -1,45 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import '../cssFile/notification.css';
+import {useGameContext} from '../context/gameContext';
 import { Cross } from '../components/svg';
+import {startgame} from '../hooks/startgame';
 import { usesocketIoContext } from '../context/socketIo';
 const Notification = () => {
-  const [reqUser, setReqUser] = useState([])
+  const {game,setGame} = useGameContext();
+ const {isAcceptORCancel,hideShow} = startgame()
   const { socket } = usesocketIoContext();
-  const [show, setShow] = useState({
-    parent: true,
-    Children: false
-  })
-  const hideShow = ({ which }) => {
-    if (which === "parent") {
-      return setShow({ ...show, parent: !parent })
-    }
-  }
-  const isAcceptORCancel = ({ isyes, id }) => {
-    if (isyes === "yes") {
-      const userName = JSON.parse(localStorage.getItem('demo-chat-user')).fullName;
-      socket?.emit("isAccept", { istrue: "yes", id, Name: userName });
-
-    }
-  }
-
   useEffect(() => {
-    socket?.on("isrequest", async ({ name, profilePic, id }) => {
-      setReqUser((prevReqUser) => [...prevReqUser, { name, profilePic, id }]);
-    })
+    if(game.show){
+      socket?.on("isrequest", async ({ name, profilePic, id }) => {
+        setGame(prevState => ({ ...prevState, notification:true, notifiyUser: [...game.notifiyUser, { name, profilePic, id }] }));
+      })
+    }
     return () => {
-      socket?.off("isAccept")
+      socket?.off("isrequest")
     }
   }, [socket])
   return (
     <>
       <div className='main-container-for-animation'>
-        {show.parent && reqUser.length !== 0 && <div className='notification-container'>
+        {game.notification && game.notifiyUser.length !== 0 && <div className='notification-container'>
           <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
             <span style={{ fontSize: 20, marginLeft: 5, color: 'white' }}>Ask to join Game</span>
             <label style={{ display: 'flex' }} onClick={e => hideShow({ which: "parent" })}><Cross height={20} width={20} /></label>
           </div>
           <div className='notify-child-container'>
-            {reqUser && reqUser.map((item, index) => (<div className='notify-child'>
+            {game.notifiyUser && game.notifiyUser.map((item, index) => (<div className='notify-child'>
               <div className='first-notify-child'>
                 <div className='User-profile-container-name-last-message'>
                   <img src={item.profilePic} />
@@ -48,7 +36,7 @@ const Notification = () => {
                 </div>
               </div>
               <div className='button-container'>
-                <button onClick={e => isAcceptORCancel({ isyes: "yes", id: item.id })}>Yes</button>
+                <button onClick={e => isAcceptORCancel({ isyes: "yes", requesteduserid: item.id,opponentName:item.name})}>Yes</button>
                 <button onClick={e => isAcceptORCancel("No")}>No</button>
               </div>
             </div>))}
